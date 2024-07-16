1f8196e7681e24dc5c1063dba63ddceb5adc5c31
Brandon Angione <110564707+BrandonAngione@users.noreply.github.com>
2024-07-16 18:39:58 -0400

    Update stock_dividend.py
    Fixed minor errors:
         2 typos:
                       Forgot the 't' in investment in the 'variable initial_investment'
                       Forgot the 't' in reinvestment in the 'variable reinvestment_dividend'
           2 syntax errors:
                       Missing comma in line 12 that was supposed to be between the string and variable 'quarters'.
                        Missing comma in line 13 that was supposed to be between the string and variable 'reinvestment_dividend'.
            2 Grammar fixes: 
                        Changed numbers to number in the strings in line 12 & 13.
             2 spelling errors:
                         Misspelled initial in the variable 'initial_investment'  in both occurrences in line 1 and line 3 by missing the third 'i'.

--- stock_dividend.py
+++ stock_dividend.py
@@ -1,13 +1,13 @@
-inital_invesment = 128000 # inital invesment, implement input in next change.
+initial_investment = 128000 # initial investment, implement input in next change.
 quarterly_dividend = 0.61 * 80 # dividend per share * share price, implement inputs in next change.
-investment_amount = inital_investment 
+investment_amount = initial_investment 
 
 quarters = 0
 while investment_amount < 1000000: # replace 1,000,000 with input.
   reinvestment_dividend = investment_amount / 80 * 0.61
   investment_amount += reinvestment_dividend
   quarters += 1
-  years = quarters /4
-print("Numbers of years to reach $1,000,000:",years)
-print("Numbers of quarters to reach $1,000,000:"quarters)
-print("how big the reinvesment dividend is at $1,000,000:"reinvesment_dividend)
+  years = quarters / 4
+print("Number of years to reach $1,000,000:", years)
+print("Number of quarters to reach $1,000,000:", quarters)
+print("How big the reinvestment dividend is at $1,000,000:", reinvestment_dividend)
