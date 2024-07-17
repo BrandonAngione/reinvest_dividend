617cf75b685eb2fa3f2cf0f4f25cca4085237586
Brandon Angione <110564707+BrandonAngione@users.noreply.github.com>
2024-07-17 17:58:57 -0400

    Update stock_dividend.py
    Added  variables at lines 3 & 5:  stock_price and investment_goal to allow for user input.
    Changed the variable values at lines 1 & 2: initial_investment and quarterly_dividend no longer have static values and take  float input instead.
    Changed the while loop at lines 8 & 9 to allow for user input.
    Changed the print functions at lines 13-15 to have it print the user's goal instead of the static value of 1 million.

--- stock_dividend.py
+++ stock_dividend.py
@@ -1,13 +1,15 @@
-initial_investment = 128000 # initial investment, implement input in next change.
-quarterly_dividend = 0.61 * 80 # dividend per share * share price, implement inputs in next change.
+initial_investment = float(input("Enter your initial investment : ")) # initial investment
+quarterly_dividend = float(input("Enter quarterly dividend : "))
+stock_price = float(input("Enter stock price : "))
 investment_amount = initial_investment 
+investment_goal = float(input("Enter your investment goal : "))
 
 quarters = 0
-while investment_amount < 1000000: # replace 1,000,000 with input.
-  reinvestment_dividend = investment_amount / 80 * 0.61
+while investment_amount < investment_goal:
+  reinvestment_dividend = investment_amount / stock_price * quarterly_dividend   # dividend per share * share price
   investment_amount += reinvestment_dividend
   quarters += 1
   years = quarters / 4
-print("Number of years to reach $1,000,000:", years)
-print("Number of quarters to reach $1,000,000:", quarters)
-print("How big the reinvestment dividend is at $1,000,000:", reinvestment_dividend)
+print("Number of years to reach", investment_goal,":", years)
+print("Number of quarters to reach", investment_goal,":", quarters)
+print("How big the reinvestment dividend, is at", investment_goal,":", reinvestment_dividend)
